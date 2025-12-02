aa67a7c16a7d1e02a01cadb35ef18e2602a5c72d
Ben Doggett <bdoggett@gmail.com>
2025-12-02 16:25:35 -0500

    Add favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,21 @@ export const metadata: Metadata = {
     type: "website",
   },
   icons: {
-    icon: "/favicon.ico",
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+    other: [
+      {
+        rel: "android-chrome-192x192",
+        url: "/android-chrome-192x192.png",
+      },
+      {
+        rel: "android-chrome-512x512",
+        url: "/android-chrome-512x512.png",
+      },
+    ],
   },
 };
 
